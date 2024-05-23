9006251cb24fc7bd51fe6e76523f892af7e3d859
Carlos Oliveira <carlos_bla@hotmail.com>
2024-05-23 16:17:09 -0300

    Add project files.

---
+++ PackageCreator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+
+namespace PackageCreator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Inform All In One solution path: ");
+                string? solutionPath = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(solutionPath))
+                    Console.WriteLine("A path must be informed");
+                else
+                {
+                    CreateNupkg(solutionPath);
+                    PublishNupkg(solutionPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        protected static void CreateNupkg(string solutionPath)
+        {
+            string packageDirectory = Path.Combine(solutionPath, "NugetPackages");
+
+            if (!Directory.Exists(packageDirectory))
+                Directory.CreateDirectory(packageDirectory);
+
+            Process.Start("CMD.exe"
+                , @$"/C dotnet pack {Path.Combine(solutionPath, "AllInOne.sln")} --output {packageDirectory}")
+                    .WaitForExit();
+
+            Console.WriteLine($"Nuget packages was created in {packageDirectory} successfully");
+
+        }
+
+        protected static void PublishNupkg(string solutionPath)
+        {
+            Console.WriteLine("Inform nuget feed url: ");
+            string? nugetFeed = Console.ReadLine();
+            string[] packages = Directory.GetFiles(Path.Combine(solutionPath, "NugetPackages"), "*.nupkg");
+            int count = 0;
+
+            foreach (string package in packages)
+            {
+                Process.Start("CMD.exe"
+                    , @$"/C dotnet nuget push --source {nugetFeed} --api-key Dummy {package} ")
+                        .WaitForExit();
+
+                count++;
+            }
+
+            Console.WriteLine($"{count}/{packages.Length} nuget packages was published successfully");
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,5 @@
+Its mandatory install Artifact Credential Provider to publish nuget packages, this tool can be found here -> https://github.com/microsoft/artifacts-credprovider
+
+or, if using windows you can open powershell as admin and execute the follow command bellow
+
+iex "& { $(irm https://aka.ms/install-artifacts-credprovider.ps1) }"
